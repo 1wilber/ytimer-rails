48d8553415bfc0c4089e2f7f0d97e54976a01c5d
1wilber <wilber@hey.com>
2024-01-25 22:32:10 -0300

    add bulma styles to sessions/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :pages, only: [:index]
-  root "pages#index"
-  devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions"}
+
+  root "timer#index"
+
+  resources :pages, only: [:index]
+  devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
